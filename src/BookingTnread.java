f1862f56e98b5342a12eb43218aa6a4544218cab
laith-prog <lthjawish@gmail.com>
2024-01-01 15:03:39 +0300

    nothing4

--- src/BookingTnread.java
+++ src/BookingTnread.java
@@ -4,53 +4,73 @@ import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.util.Random;
 
-public class BookingTnread implements Runnable{
+public class BookingTnread implements Runnable {
     JFrame jframe;
     Ticket ticket;
 
 
-    public BookingTnread(JFrame jframe,Ticket ticket) {
+    public BookingTnread(JFrame jframe, Ticket ticket) {
         this.jframe = jframe;
-        this.ticket=ticket;
+        this.ticket = ticket;
     }
 
     @Override
     public void run() {
+        try {
+            PrintWriter writer = new PrintWriter(new FileOutputStream("ticketID.txt"));
 
-        for (int j=0;j<ticket.seatNum;j++) {String alphabet = "ABCDEFGH0123456789";
+            for (int j = 0; j < ticket.seatNum; j++) {
+                String alphabet = "ABCDEFGH0123456789";
 
-            // create random string builder
-            StringBuilder sb = new StringBuilder();
+                // create random string builder
+                StringBuilder sb = new StringBuilder();
 
-            // create an object of Random class
-            Random random = new Random();
+                // create an object of Random class
+                Random random = new Random();
 
-            // specify length of random string
-            int length = 5;
+                // specify length of random string
+                int length = 5;
+                StringBuilder userTicketID = new StringBuilder();
 
-            for (int i = 0; i < length; i++) {
-                // generate random index number
-                int index = random.nextInt(alphabet.length());
+                for (int i = 0; i < length; i++) {
+                    // generate random index number
+                    int index = random.nextInt(alphabet.length());
 
-                // get character specified by index
-                // from the string
-                char randomChar = alphabet.charAt(index);
+                    // get character specified by index
+                    // from the string
+                    char randomChar = alphabet.charAt(index);
 
-                // append the character to string builder
-                sb.append(randomChar);
-            }
-            ticket.ticketID = sb.toString();
-            JOptionPane.showMessageDialog(jframe, "Your ticket's ID is: " + ticket.ticketID);
-            try {
-                PrintWriter writer = new PrintWriter(new FileOutputStream("ticketID.txt"));
-                writer.println("ID: " + ticket.getTicketID());
-                writer.close();
-            } catch (FileNotFoundException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
+                    // append the character to string builder
+                    sb.append(randomChar);
+                    if (sb.length() == 5) {
+                        userTicketID.append(sb);
+                        writer.println("ID: " + userTicketID);
+                        System.out.println(userTicketID);
 
-            jframe.dispose();
-        }                // create a string of all characters
+                    }
+
+
+                }
+
+
+                JOptionPane.showMessageDialog(jframe, "Your ticket's ID is: " + userTicketID);
+
+
+                jframe.dispose();
+            }                // create a string of all characters
+
+
+
+
+            writer.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+
     }
 }
--- src/CacnelingThread.java
+++ src/CacnelingThread.java
@@ -1,24 +1,71 @@
 import javax.swing.*;
+import java.io.*;
+import java.util.Scanner;
 
-public class CacnelingThread implements Runnable{
+public class CacnelingThread implements Runnable {
     JFrame jframe;
     Ticket ticket;
 
 
-    public CacnelingThread(JFrame jframe,Ticket ticket) {
+    public CacnelingThread(JFrame jframe, Ticket ticket) {
         this.jframe = jframe;
-        this.ticket=ticket;
+        this.ticket = ticket;
     }
 
     @Override
     public void run() {
-        System.out.println(ticket.ticketID);
         String userTicketID;
         userTicketID = JOptionPane.showInputDialog(jframe, "Enter your ticket ID: ");
-        if (userTicketID.isBlank() || !userTicketID.equals(ticket.ticketID)) {
-            JOptionPane.showMessageDialog(jframe, "Ticket does not exist");
-        } else {
-            JOptionPane.showMessageDialog(jframe, "Booking cancelled");
+
+
+        System.out.println(ticket.ticketID);
+        try {
+            FileReader freader = new FileReader("ticketID.txt");
+
+            Scanner scanner = new Scanner(freader);
+            FileWriter writer = new FileWriter("temp.txt");
+            while (scanner.hasNextLine()) {
+
+
+                String line = scanner.nextLine();
+
+
+                // compare the line with the ID
+                if (line.equals("ID: " + userTicketID)) {
+                    System.out.println("helllllo");
+
+
+                    while (scanner.hasNextLine()) {
+                        line = scanner.nextLine();
+
+                        if (!line.equals("ID: " + userTicketID)) {
+                            writer.write(line + "\n");
+                            System.out.println("helllllo");
+                        }
+                    }
+                    JOptionPane.showMessageDialog(jframe, "This ticket has been cancelled");
+                    break;
+                } else if (userTicketID.isBlank() ||!line.equals("ID: " + userTicketID)) {
+                    JOptionPane.showMessageDialog(jframe, "Ticket does not exist");
+                    break;
+
+                }
+            }
+            scanner.close();
+            writer.close();
+            File file = new File("ticketID.txt");
+            file.delete();
+
+
+// rename the temporary file to the original file
+            File temp = new File("temp.txt");
+            temp.renameTo(file);
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
